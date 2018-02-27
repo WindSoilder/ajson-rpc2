4008f8c916a4f0e9465c614fc1b9aba72c4ee5b2
keith <WindSoilder@outlook.com>
2018-02-28 01:51:05 +0800

    update readme

--- tests/context.py
+++ tests/context.py
@@ -2,7 +2,6 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from ajson_rpc2 import utils
 from ajson_rpc2 import server
 
 from ajson_rpc2.utils import (
