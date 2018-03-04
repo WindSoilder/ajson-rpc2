8c7601f7aa24d165127a27a183cfa8028ce2c4df
keith <WindSoilder@outlook.com>
2018-03-04 22:00:36 +0800

    test

--- accept_tests/accept_test.py
+++ accept_tests/accept_test.py
@@ -56,6 +56,8 @@ def close_server_process(process: Process):
 
 def main():
     process = start_server_process()
+    import time
+    time.sleep(3)
     fails = run_client_tests()
     if len(fails) == 0:
         print("all tests run complete with no errors")
